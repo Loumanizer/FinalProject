e78301b1b5c319aad53b29a82c3138ad5cb59968
Pranab Shrestha <molly.shrestha@gmail.com>
2017-11-25 14:36:22 -0500

    So far sms also working

--- CollegeLibrarySystem/app/src/main/java/shova/shrestha/edu/oakland/collegelibrarysystem/StudentDetail.java
+++ CollegeLibrarySystem/app/src/main/java/shova/shrestha/edu/oakland/collegelibrarysystem/StudentDetail.java
@@ -3,11 +3,15 @@ package shova.shrestha.edu.oakland.collegelibrarysystem;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import com.activeandroid.query.Select;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +20,7 @@ public class StudentDetail extends AppCompatActivity {
 
     StringAdapter ckadapter = null;
     BookCheckoutAdapter bookckoutdapter = null;
-	
+
     TextView textDsplyViewstudentName  = null;
     TextView textDsplyViewStudentPhoneNumb= null;
     ListView listviewStudentCheckout = null;
@@ -39,14 +43,10 @@ public class StudentDetail extends AppCompatActivity {
         btnStudentCheckIn = (Button)findViewById(R.id.btnstudentsendSms) ;
         btnStudentSendSms = (Button)findViewById(R.id.btnstudentsendChkIn);
 
-        ckadapter = new StringAdapter(this);
-        bookckoutdapter = new BookCheckoutAdapter();
-
-        listviewStudentCheckout.setAdapter(ckadapter);
-        listviewStudentCheckout.setAdapter(bookckoutdapter);
 
         Intent intent = getIntent();
         int _studentId = intent.getIntExtra("STUDENT_ID", 0);
+        textDsplyViewstudentName.setTag(_studentId);
         textDsplyViewstudentName.setText(dbStudentHelper.getStudentName(_studentId));
         textDsplyViewStudentPhoneNumb.setText(dbStudentHelper.getStudentPhoneNumber(_studentId));
 
@@ -57,17 +57,49 @@ public class StudentDetail extends AppCompatActivity {
         View parentRow = (View) view.getParent();
         ListView listView = (ListView) parentRow.getParent();
         final int position = listView.getPositionForView(parentRow);
+        BookCheckoutAdapter _bookckoutdapter_= (BookCheckoutAdapter) listView.getAdapter();
+        int ckid = _bookckoutdapter_.getCheckoutId(position);
+        Select query = new Select();
+        dbCheckoutHelper choutdb = query.from(dbCheckoutHelper.class)
+                .where("id = ?", ckid)
+                .executeSingle();
+        choutdb.delete();
+
+        clsCheckoutupdateList((int) textDsplyViewstudentName.getTag());
+        clsCheckoutclearForm();
+    }
+
+    public void btnStudentSendSms(View view){
+        View parentRow = (View) view.getParent();
+        ListView listView = (ListView) parentRow.getParent();
+        final int position = listView.getPositionForView(parentRow);
+        BookCheckoutAdapter _bookckoutdapter = (BookCheckoutAdapter) listView.getAdapter();
+        String _studedntPhone = textDsplyViewStudentPhoneNumb.getText().toString();
+        String _bookName = _bookckoutdapter.getCheckoutBookName(position);
+        String _duedate = _bookckoutdapter.getCheckoutDuedate(position);
+        String _smsmsg = "Please return **"  + _bookName + " ** book. Which is due on : " + _duedate;
+        // logic to send SMS
+
+        SmsManager smsManager = SmsManager.getDefault();
+        smsManager.sendTextMessage(_studedntPhone, null, _smsmsg, null, null);
     }
 
     private void clsCheckoutupdateList(int _studentId)
     {
+        ckadapter = new StringAdapter(this);
+        bookckoutdapter = new BookCheckoutAdapter();
+
+        listviewStudentCheckout.setAdapter(ckadapter);
+        listviewStudentCheckout.setAdapter(bookckoutdapter);
+
         List<dbCheckoutHelper> _stdcheckoutlist = dbCheckoutHelper.getStudentAllCheckout(_studentId);
         int tablesize = _stdcheckoutlist.size();
         for (int _index = 0; _index < tablesize; _index++) {
             int Bookid = _stdcheckoutlist.get(_index).bookId;
             String _bookName = dbBookHelper.getBookName(Bookid);
             String _duedate = _stdcheckoutlist.get(_index).dueDate;
-            bookckoutdapter.addCheckoutBook(_bookName, _duedate);
+            int _ckout = _stdcheckoutlist.get(_index).getId().intValue();
+            bookckoutdapter.addCheckoutBook(_ckout, _bookName, _duedate);
         }
         listviewStudentCheckout.setAdapter(bookckoutdapter);
     }
